7ae632fc44336d675a8d47685fe86a880df6bdef
mziya <mziyag5187@gmail.com>
2022-12-15 14:59:27 +0300

    conflict commit from Ziya branch

---
+++ src/Conflict.java
@@ -0,0 +1,10 @@
+public class Conflict {
+    public static void main(String[] args) {
+        System.out.println("conflict change from Ziya branch");
+        System.out.println("conflict change from Ziya branch");
+        System.out.println("conflict change from Ziya branch");
+        System.out.println("conflict change from Ziya branch");
+        System.out.println("conflict change from Ziya branch");
+        System.out.println("conflict change from Ziya branch");
+    }
+}
