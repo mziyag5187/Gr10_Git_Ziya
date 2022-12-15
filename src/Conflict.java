ede37b3d65a41c978eda136381a276a6848f9425
mziya <62723491+mziyag5187@users.noreply.github.com>
2022-12-15 14:49:06 +0300

    Create Conflict.java

---
+++ src/Conflict.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("conflict change from Ziya branch");
+        System.out.println("conflict change from Ziya branch");
+        System.out.println("conflict change from Ziya branch");
+        System.out.println("conflict change from Ziya branch");
+        System.out.println("conflict change from Ziya branch");
+        System.out.println("conflict change from Ziya branch");
+        System.out.println("conflict change from Ziya branch");      
+    }
+}
